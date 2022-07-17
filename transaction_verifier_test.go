e0c574b3b34788e3a27f6e861da96902583ca538
Almog Tal <almog.tal@algorand.com>
2022-07-17 13:35:38 +0300

    Added encoded assets for transaction verification

--- transaction_verifier_test.go
+++ transaction_verifier_test.go
@@ -1,83 +1,44 @@
 package main
 
 import (
-	"encoding/base64"
-	"encoding/json"
 	"testing"
 
 	"github.com/algorand/go-algorand-sdk/client/v2/common/models"
 	"github.com/stretchr/testify/require"
 )
 
-func getTransactionId() ([]byte, error) {
-	transactionIdEncoded := "DQA86GJmEfXKpOCFtbW31EYoFqSjRR8/t63RGCajkHA="
-	return base64.StdEncoding.DecodeString(transactionIdEncoded)
-}
-
-func getTransactionProofResponse() (models.ProofResponse, error) {
-	proofJsonStr := `{
-"hashtype": "sha256",
-"idx": 0,
-"proof": "5xvqtS3zHjSUWiBUK8YupN8AXvl733os1Xln8RfEZBA=",
-"stibhash": "f8mCwgwSThJwH37N5hBj6CjdO/ZhH8XXby2/Bke1lN0=",
-"treedepth": 1
-}`
-	jsonBytes := []byte(proofJsonStr)
-	resp := models.ProofResponse{}
-
-	err := json.Unmarshal(jsonBytes, &resp)
-	return resp, err
-}
-
-func getLightBlockHeaderProofResponse() (LightBlockHeaderProofResponse, error) {
-	proofJsonStr := `{
-"index": 0,
-"proof": "oNr1sknaf3Hb9rohvNMVRt+LVg71Q3bQa+Fn7u9IRDoQEwCtQSIdOKhrmmubtJq5l7PFaH452Og/xPKqCKySCje8rezV8J56Znxge8MTaF66c6NYOKbgrDq7OvCUiYjX",
-"treedepth": 3
-}`
-	jsonBytes := []byte(proofJsonStr)
-	resp := LightBlockHeaderProofResponse{}
-
-	err := json.Unmarshal(jsonBytes, &resp)
-	return resp, err
-}
-
-func getBlockIntervalCommitment() ([]byte, error) {
-	commitmentStr := "0QgCvCujCapNmpxTiVv5meq3WLfA3R8G857/nF0iyF0="
-	return base64.StdEncoding.DecodeString(commitmentStr)
-}
-
-func getGenesisHash() ([]byte, error) {
-	genesisHashEncoded := "SvwlI5kslp0rKzWBFTxeIdp6nxxxZa97LJx03F39bEQ="
-	return base64.StdEncoding.DecodeString(genesisHashEncoded)
-}
-
-func getRound() uint64 {
-	return 9
-}
+var transactionVerificationFolder = "encoded_assets/transaction_verification/"
 
 func TestTransactionVerify(t *testing.T) {
 	r := require.New(t)
-	transactionId, err := getTransactionId()
+
+	var genesisHash []byte
+	err := decodeFromFile(transactionVerificationFolder+"genesis_hash.json", &genesisHash)
 	r.NoError(err)
 
-	transactionProof, err := getTransactionProofResponse()
+	var round uint64
+	err = decodeFromFile(transactionVerificationFolder+"round.json", &round)
 	r.NoError(err)
 
-	lightBlockHeaderProof, err := getLightBlockHeaderProofResponse()
+	var transactionId []byte
+	err = decodeFromFile(transactionVerificationFolder+"transaction_id.json", &transactionId)
 	r.NoError(err)
 
-	// Will be global
-	blockIntervalCommitment, err := getBlockIntervalCommitment()
+	var transactionProofResponse models.ProofResponse
+	err = decodeFromFile(transactionVerificationFolder+"transaction_proof_response.json", &transactionProofResponse)
 	r.NoError(err)
 
-	genesisHash, err := getGenesisHash()
+	var lightBlockHeaderProofResponse LightBlockHeaderProofResponse
+	err = decodeFromFile(transactionVerificationFolder+"light_block_header_proof_response.json", &lightBlockHeaderProofResponse)
 	r.NoError(err)
 
-	round := getRound()
+	// TODO: Get this from the state proof trusted data
+	var lightBlockHeaderCommitment []byte
+	err = decodeFromFile(transactionVerificationFolder+"light_block_header_commitment.json", &lightBlockHeaderCommitment)
+	r.NoError(err)
 
-	verified, err := VerifyTransaction(transactionId, transactionProof,
-		lightBlockHeaderProof, blockIntervalCommitment, genesisHash, round)
+	verified, err := VerifyTransaction(transactionId, transactionProofResponse,
+		lightBlockHeaderProofResponse, lightBlockHeaderCommitment, genesisHash, round)
 	r.NoError(err)
 	if !verified {
 		t.Fail()
