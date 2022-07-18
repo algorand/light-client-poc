53f10f4f59187b8935a0ada1d57ccfa7d3919a2b
Almog Tal <almog.tal@algorand.com>
2022-07-18 18:38:22 +0300

    added correct encoded assets

--- encoded_assets/parsed_encoded_data.go
+++ encoded_assets/parsed_encoded_data.go
@@ -3,7 +3,7 @@ package encoded_assets
 import (
 	"github.com/algorand/go-algorand-sdk/client/v2/common/models"
 	"github.com/algorand/go-algorand-sdk/stateproofs/datatypes"
-	"github.com/algorand/go-algorand/crypto/stateproof"
+	"github.com/algorand/go-algorand-sdk/types"
 	"light-client-poc/utilities"
 )
 
@@ -11,77 +11,77 @@ var transactionVerificationFolder = "encoded_assets/transaction_verification/"
 var stateProofVerificationFolder = "encoded_assets/state_proof_verification/"
 var generalAssetsFolder = "encoded_assets/general_assets/"
 
-func GetGenesisHash() (*[]byte, error) {
-	var genesisHash []byte
+func GetGenesisHash() (types.Digest, error) {
+	var genesisHash types.Digest
 	err := utilities.DecodeFromFile(generalAssetsFolder+"genesis_hash.json", &genesisHash)
 	if err != nil {
-		return nil, err
+		return types.Digest{}, err
 	}
 
-	return &genesisHash, nil
+	return genesisHash, nil
 }
 
-func GetParsedTransactionVerificationData() (uint64, *[]byte, *models.ProofResponse,
-	*LightBlockHeaderProofResponse, *[]byte, error) {
-	var round uint64
+func GetParsedTransactionVerificationData() (types.Round, types.Digest, models.ProofResponse,
+	models.LightBlockHeaderProof, types.Digest, error) {
+	var round types.Round
 	err := utilities.DecodeFromFile(transactionVerificationFolder+"round.json", &round)
 	if err != nil {
-		return 0, nil, nil, nil, nil, err
+		return 0, types.Digest{}, models.ProofResponse{}, models.LightBlockHeaderProof{}, types.Digest{}, err
 	}
 
-	var transactionId []byte
+	var transactionId types.Digest
 	err = utilities.DecodeFromFile(transactionVerificationFolder+"transaction_id.json", &transactionId)
 	if err != nil {
-		return 0, nil, nil, nil, nil, err
+		return 0, types.Digest{}, models.ProofResponse{}, models.LightBlockHeaderProof{}, types.Digest{}, err
 	}
 
 	var transactionProofResponse models.ProofResponse
 	err = utilities.DecodeFromFile(transactionVerificationFolder+"transaction_proof_response.json", &transactionProofResponse)
 	if err != nil {
-		return 0, nil, nil, nil, nil, err
+		return 0, types.Digest{}, models.ProofResponse{}, models.LightBlockHeaderProof{}, types.Digest{}, err
 	}
 
-	var lightBlockHeaderProofResponse LightBlockHeaderProofResponse
-	err = utilities.DecodeFromFile(transactionVerificationFolder+"light_block_header_proof_response.json", &lightBlockHeaderProofResponse)
+	var lightBlockHeaderProof models.LightBlockHeaderProof
+	err = utilities.DecodeFromFile(transactionVerificationFolder+"light_block_header_proof_response.json", &lightBlockHeaderProof)
 	if err != nil {
-		return 0, nil, nil, nil, nil, err
+		return 0, types.Digest{}, models.ProofResponse{}, models.LightBlockHeaderProof{}, types.Digest{}, err
 	}
 
-	var lightBlockHeaderCommitment []byte
+	var lightBlockHeaderCommitment types.Digest
 	err = utilities.DecodeFromFile(transactionVerificationFolder+"light_block_header_commitment.json", &lightBlockHeaderCommitment)
 	if err != nil {
-		return 0, nil, nil, nil, nil, err
+		return 0, types.Digest{}, models.ProofResponse{}, models.LightBlockHeaderProof{}, types.Digest{}, err
 	}
 
-	return round, &transactionId, &transactionProofResponse,
-		&lightBlockHeaderProofResponse, &lightBlockHeaderCommitment, nil
+	return round, transactionId, transactionProofResponse,
+		lightBlockHeaderProof, lightBlockHeaderCommitment, nil
 }
 
-func GetParsedStateProofAdvancmentData() (*datatypes.GenericDigest, uint64, *datatypes.Message,
-	*stateproof.StateProof, error) {
+func GetParsedStateProofAdvancmentData() (datatypes.GenericDigest, uint64, datatypes.Message,
+	*datatypes.EncodedStateProof, error) {
 	genesisVotersCommitment := datatypes.GenericDigest{}
 	err := utilities.DecodeFromFile(stateProofVerificationFolder+"genesis_voters_commitment.json", &genesisVotersCommitment)
 	if err != nil {
-		return nil, 0, nil, nil, err
+		return nil, 0, datatypes.Message{}, nil, err
 	}
 
 	genesisVotersLnProvenWeight := uint64(0)
 	err = utilities.DecodeFromFile(stateProofVerificationFolder+"genesis_voters_ln_proven_weight.json", &genesisVotersLnProvenWeight)
 	if err != nil {
-		return nil, 0, nil, nil, err
+		return nil, 0, datatypes.Message{}, nil, err
 	}
 
 	stateProofMessage := datatypes.Message{}
 	err = utilities.DecodeFromFile(stateProofVerificationFolder+"state_proof_message.json", &stateProofMessage)
 	if err != nil {
-		return nil, 0, nil, nil, err
+		return nil, 0, datatypes.Message{}, nil, err
 	}
 
-	stateProof := stateproof.StateProof{}
+	var stateProof datatypes.EncodedStateProof
 	err = utilities.DecodeFromFile(stateProofVerificationFolder+"state_proof.json", &stateProof)
 	if err != nil {
-		return nil, 0, nil, nil, err
+		return nil, 0, datatypes.Message{}, nil, err
 	}
 
-	return &genesisVotersCommitment, genesisVotersLnProvenWeight, &stateProofMessage, &stateProof, nil
+	return genesisVotersCommitment, genesisVotersLnProvenWeight, stateProofMessage, &stateProof, nil
 }
