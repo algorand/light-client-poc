9ee63660187c56cb7ba864028050001d625074e1
Almog Tal <almog.tal@algorand.com>
2022-07-26 12:19:16 +0300

    Now using relative paths to decode committed data

--- encoded_assets/parsedEncodedData.go
+++ encoded_assets/parsedEncodedData.go
@@ -4,53 +4,56 @@ import (
 	"github.com/algorand/go-algorand-sdk/client/v2/common/models"
 	"github.com/algorand/go-algorand-sdk/stateproofs/stateprooftypes"
 	"github.com/algorand/go-algorand-sdk/types"
+	"path/filepath"
 
 	"github.com/almog-t/light-client-poc/utilities"
 )
 
-var transactionVerificationFolder = "encoded_assets/transaction_verification/"
-var stateProofVerificationFolder = "encoded_assets/state_proof_verification/"
-
 func GetParsedTransactionVerificationData() (types.Digest, types.Round, stateprooftypes.Seed, types.Digest, models.ProofResponse,
 	models.LightBlockHeaderProof, types.Digest, error) {
+	transactionVerificationFolder, err := filepath.Abs(filepath.Join("encoded_assets", "transaction_verification"))
+	if err != nil {
+		return types.Digest{}, 0, stateprooftypes.Seed{}, types.Digest{}, models.ProofResponse{}, models.LightBlockHeaderProof{}, types.Digest{}, err
+	}
+
 	var genesisHash types.Digest
-	err := utilities.DecodeFromFile(transactionVerificationFolder+"genesis_hash.json", &genesisHash)
+	err = utilities.DecodeFromFile(filepath.Join(transactionVerificationFolder, "genesis_hash.json"), &genesisHash)
 	if err != nil {
 		return types.Digest{}, 0, stateprooftypes.Seed{}, types.Digest{}, models.ProofResponse{}, models.LightBlockHeaderProof{}, types.Digest{}, err
 	}
 
 	var round types.Round
-	err = utilities.DecodeFromFile(transactionVerificationFolder+"round.json", &round)
+	err = utilities.DecodeFromFile(filepath.Join(transactionVerificationFolder, "round.json"), &round)
 	if err != nil {
 		return types.Digest{}, 0, stateprooftypes.Seed{}, types.Digest{}, models.ProofResponse{}, models.LightBlockHeaderProof{}, types.Digest{}, err
 	}
 
 	var seed stateprooftypes.Seed
-	err = utilities.DecodeFromFile(transactionVerificationFolder+"seed.json", &seed)
+	err = utilities.DecodeFromFile(filepath.Join(transactionVerificationFolder, "seed.json"), &seed)
 	if err != nil {
 		return types.Digest{}, 0, stateprooftypes.Seed{}, types.Digest{}, models.ProofResponse{}, models.LightBlockHeaderProof{}, types.Digest{}, err
 	}
 
 	var transactionId types.Digest
-	err = utilities.DecodeFromFile(transactionVerificationFolder+"transaction_id.json", &transactionId)
+	err = utilities.DecodeFromFile(filepath.Join(transactionVerificationFolder, "transaction_id.json"), &transactionId)
 	if err != nil {
 		return types.Digest{}, 0, stateprooftypes.Seed{}, types.Digest{}, models.ProofResponse{}, models.LightBlockHeaderProof{}, types.Digest{}, err
 	}
 
 	var transactionProofResponse models.ProofResponse
-	err = utilities.DecodeFromFile(transactionVerificationFolder+"transaction_proof_response.json", &transactionProofResponse)
+	err = utilities.DecodeFromFile(filepath.Join(transactionVerificationFolder, "transaction_proof_response.json"), &transactionProofResponse)
 	if err != nil {
 		return types.Digest{}, 0, stateprooftypes.Seed{}, types.Digest{}, models.ProofResponse{}, models.LightBlockHeaderProof{}, types.Digest{}, err
 	}
 
 	var lightBlockHeaderProof models.LightBlockHeaderProof
-	err = utilities.DecodeFromFile(transactionVerificationFolder+"light_block_header_proof_response.json", &lightBlockHeaderProof)
+	err = utilities.DecodeFromFile(filepath.Join(transactionVerificationFolder, "light_block_header_proof_response.json"), &lightBlockHeaderProof)
 	if err != nil {
 		return types.Digest{}, 0, stateprooftypes.Seed{}, types.Digest{}, models.ProofResponse{}, models.LightBlockHeaderProof{}, types.Digest{}, err
 	}
 
 	var lightBlockHeaderCommitment types.Digest
-	err = utilities.DecodeFromFile(transactionVerificationFolder+"light_block_header_commitment.json", &lightBlockHeaderCommitment)
+	err = utilities.DecodeFromFile(filepath.Join(transactionVerificationFolder, "light_block_header_commitment.json"), &lightBlockHeaderCommitment)
 	if err != nil {
 		return types.Digest{}, 0, stateprooftypes.Seed{}, types.Digest{}, models.ProofResponse{}, models.LightBlockHeaderProof{}, types.Digest{}, err
 	}
@@ -61,26 +64,31 @@ func GetParsedTransactionVerificationData() (types.Digest, types.Round, statepro
 
 func GetParsedStateProofAdvancmentData() (stateprooftypes.GenericDigest, uint64, stateprooftypes.Message,
 	*stateprooftypes.EncodedStateProof, error) {
+	stateProofVerificationFolder, err := filepath.Abs(filepath.Join("encoded_assets", "state_proof_verification"))
+	if err != nil {
+		return nil, 0, stateprooftypes.Message{}, nil, err
+	}
+
 	genesisVotersCommitment := stateprooftypes.GenericDigest{}
-	err := utilities.DecodeFromFile(stateProofVerificationFolder+"genesis_voters_commitment.json", &genesisVotersCommitment)
+	err = utilities.DecodeFromFile(filepath.Join(stateProofVerificationFolder, "genesis_voters_commitment.json"), &genesisVotersCommitment)
 	if err != nil {
 		return nil, 0, stateprooftypes.Message{}, nil, err
 	}
 
 	genesisVotersLnProvenWeight := uint64(0)
-	err = utilities.DecodeFromFile(stateProofVerificationFolder+"genesis_voters_ln_proven_weight.json", &genesisVotersLnProvenWeight)
+	err = utilities.DecodeFromFile(filepath.Join(stateProofVerificationFolder, "genesis_voters_ln_proven_weight.json"), &genesisVotersLnProvenWeight)
 	if err != nil {
 		return nil, 0, stateprooftypes.Message{}, nil, err
 	}
 
 	stateProofMessage := stateprooftypes.Message{}
-	err = utilities.DecodeFromFile(stateProofVerificationFolder+"state_proof_message.json", &stateProofMessage)
+	err = utilities.DecodeFromFile(filepath.Join(stateProofVerificationFolder, "state_proof_message.json"), &stateProofMessage)
 	if err != nil {
 		return nil, 0, stateprooftypes.Message{}, nil, err
 	}
 
 	var stateProof stateprooftypes.EncodedStateProof
-	err = utilities.DecodeFromFile(stateProofVerificationFolder+"state_proof.json", &stateProof)
+	err = utilities.DecodeFromFile(filepath.Join(stateProofVerificationFolder, "state_proof.json"), &stateProof)
 	if err != nil {
 		return nil, 0, stateprooftypes.Message{}, nil, err
 	}
